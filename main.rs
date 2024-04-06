302e0d3444fa9c7e565803c3acef1f7e8adae63e
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-04-06 12:37:24 +0700

    task(2): mendefinisikan variable

--- main.rs
+++ main.rs
@@ -1,5 +1,11 @@
 /*fn main ini merupakan entri point untuk rust pertama kali dijalankan */
 fn main() {
     // Print `Helo kepiting di terminal`
-    println!("Hello Kepiting")
+    println!("Hello Kepiting");
+
+    // Belajar Type Data
+    // Convention untuk penulisan variable menggunakan snake case
+    let is_oke = "OK";
+    println!("Print OK >> {}", is_oke);
+    
 }
