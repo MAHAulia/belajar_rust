5ab5f93ae11fe520549146ed923a3da024616a08
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-04-06 12:40:39 +0700

    task(6); reserved word

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 // consts didefiniskan di luar main
-const NUM:i32 = 1000;
+const NUM: i32 = 1000;
 
 /*fn main ini merupakan entri point untuk rust pertama kali dijalankan */
 fn main() {
@@ -12,8 +12,8 @@ fn main() {
     println!("\nPrint OK >> {}", is_oke);
 
     // pendefinisian variable dengan tipe data
-    let counter:i32 = 0;
-    let title:String = "Current Counter".to_string();
+    let counter: i32 = 0;
+    let title: String = "Current Counter".to_string();
     println!("\n{}", title);
     println!("`{}`", counter);
 
@@ -28,9 +28,26 @@ fn main() {
     println!("\nPRINT CONST >> {}", NUM);
 
     // Convert type data
-    let variable1:f32 = 100.99;
-    let variable2:i32 = variable1 as i32;
+    let variable1: f32 = 100.99;
+    let variable2: i32 = variable1 as i32;
     println!("\n{}", variable1);
     println!("{}", variable2);
-    
+
+    // Reserved Word
+    /*
+        Reserved Words
+    abstract alignof as become box
+    break const continue crate do
+    else enum extern false final
+    fn for if impl in
+    let loop macro match mod
+    move mut
+    offsetof override priv
+    proc pub pure ref return
+    Self self sizeof static struct
+    super trait true type typeof
+    unsafe unsized use virtual where
+    while yield
+    Rust reserved words cannot be used when choosing identifier names for
+    variables, functions, properties. */
 }
